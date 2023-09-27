b8d839131620955264e10b77dc9d94306612617e
mtaylo54 <mtaylo54@jaguarlandrover.com>
2023-09-27 14:47:46 +0100

    Troubleshoot changes to firewall_ports var

--- gce-instance/firewall.tf
+++ gce-instance/firewall.tf
@@ -3,7 +3,7 @@ resource "google_compute_firewall" "firewall" {
   network = var.network
   allow {
     protocol = var.firewall_protocol
-    ports    = [var.firewall_port]
+    ports    = var.firewall_port
   }
   source_tags   = [var.name]
   source_ranges = var.firewall_source
